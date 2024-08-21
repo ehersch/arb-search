939b26ad14f1a2f4649aa62a557c5f9dedd57575
Darian Nwankwo <nwankwodarian@gmail.com>
2024-08-21 12:18:10 -0400

    cleanup

--- src/webscraper/collect_mlb_predictions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3 get_mlb_game_urls.py --url https://www.espn.com/mlb/schedule --headless --wait_class_names "ScheduleTables" > mlb_game_urls.txt
-
-# Read each line from the file and pass it to the python script
-while IFS= read -r url
-do
-    python3 get_mlb_game_matchup_predictor.py --url "$url" --headless --wait_class_names "matchupPredictor" >> match_predictions.txt
-done < mlb_game_urls.txt
